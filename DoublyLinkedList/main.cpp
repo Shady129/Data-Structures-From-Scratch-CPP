67d980db94c67b3c3b1b9181e7e3ac94239a5f6a
Shady Mahmoud <120749934+Shady129@users.noreply.github.com>
2026-02-18 06:25:21 -0800

    Update main.cpp

--- DoublyLinkedList/main.cpp
+++ DoublyLinkedList/main.cpp
@@ -1,1 +1,91 @@
+#include <iostream>
+#include "clsDblLinkedList.h"
+using namespace std;
 
+
+
+int main()
+{
+
+	clsDblLinkedList<int> myList;
+
+
+
+	myList.InsertAtBeginning(10);
+	myList.InsertAtBeginning(20);
+	myList.InsertAtBeginning(30);
+	myList.InsertAtBeginning(40);
+	myList.InsertAtBeginning(50);
+
+	cout << "List: ";
+	myList.PrintList();
+
+
+
+	cout << "First Item: " << myList.GetFirstItem() << endl;
+
+
+	cout << "Last Item: " << myList.GetLastItem() << endl;
+
+
+
+
+	
+	cout << "Size: " << myList.Size() << endl;
+	
+
+
+	cout << "\nBefore Update:\n";
+	myList.PrintList();
+
+
+	
+	if (myList.UpdateItem(0, 999))
+	{
+		cout << "\nUpdate Success.\n";
+	}
+	else
+	{
+		cout << "\nUpdate Failed.\n";
+	}
+
+	cout << "\nAfter Update:\n";
+	myList.PrintList();
+
+
+
+
+	myList.DeleteByValue(20);
+	myList.PrintList();
+	cout << "\nSize: " << myList.Size() << endl;
+
+
+
+
+	cout << "\nAfter Delete First:\n";
+	myList.DeleteFirstNode();
+	myList.PrintList();
+	cout << "Size: " << myList.Size() << endl;
+
+
+
+
+	cout << "\nAfter Delete Last:\n";
+	myList.DeleteLastNode();
+	myList.PrintList();
+	cout << "Size: " << myList.Size() << endl;
+
+
+
+
+
+	cout << "\nAfter Delete Last Again:\n";
+	myList.DeleteLastNode();
+	myList.PrintList();
+	cout << "Size: " << myList.Size() << endl;
+
+
+
+	return 0;
+
+}
